00785b6bccd2c1570bc47bcd77bb6152b9dade57
nacho <ignacioaristegui025@gmail.com>
2024-03-06 15:43:53 -0300

    Uso de react Lazy - Load para Technologies.

--- src/components/component/typingEffect.jsx
+++ src/components/component/typingEffect.jsx
@@ -1,24 +1,27 @@
 "use client";
-import React from "react"; 
-import Typewriter from 'typewriter-effect'; 
-  
-export default function TypingEffect() { 
+import React from "react";
+import LazyLoad from 'react-lazy-load';
+import Typewriter from 'typewriter-effect';
+
+export default function TypingEffect() {
   const startTypewriter = (typewriter) => {
-    typewriter.typeString('Technologies') 
-      .callFunction(() => { 
-        console.log('String typed out!'); 
-      }) 
-      .pauseFor(2000)  
-      .start(startTypewriter); 
+    typewriter.typeString('Technologies')
+      .callFunction(() => {
+        console.log('String typed out!');
+      })
+      .pauseFor(2000)
+      .start(startTypewriter);
   };
 
-  return ( 
-    <div> 
-      <h3 className="text-4xl font-bold mb-6 text-center">
-        <Typewriter 
-          onInit={startTypewriter}
-        /> 
-      </h3>
-    </div> 
-  ); 
+  return (
+    <div>
+      <LazyLoad height={200} offset={100}>
+        <h3 className="text-4xl font-bold mb-6 text-center">
+          <Typewriter
+            onInit={startTypewriter}
+          />
+        </h3>
+      </LazyLoad>
+    </div>
+  );
 }
